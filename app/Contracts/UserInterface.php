ad4d65f168287a3ecbf856a056ef7b70211df07d
Rosiliza Rioja <riojarosiliza@gmail.com>
2015-08-08 19:56:18 +0800

    add function in UserRepository (myResellers, mySuppliers)

--- app/Contracts/UserInterface.php
+++ app/Contracts/UserInterface.php
@@ -7,5 +7,7 @@ use App\Contracts\BaseInterface;
 interface UserInterface extends BaseInterface{
 
 	public function allByType($type);
+	public function myResellers($type, $userid);
+	public function mySuppliers($type, $userid);
 
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -4,15 +4,49 @@ namespace App\Repositories;
 
 use App\Repositories\BaseRepository;
 use App\Contracts\UserInterface;
+use DB;
 
 class UserRepository extends BaseRepository implements UserInterface
 {
     protected $modelName = 'App\User';
 
+    /**
+     * Get users by type
+     *
+     * @param int | supplier reseller
+     */
+
     public function allByType($type)
     {
         $instance = $this->getNewInstance();
         return $instance->whereType($type)->get();
+    }
+
+
+    public function myResellers($type, $userid)
+    {
+    	$instance = $this->getNewInstance();
+        return DB::table('users')
+        			->where('type', $type)
+        			->join('supplier_reseller', function($join) use ($userid)
+				        {
+				            $join->on('supplier_reseller.reseller_id', '=', 'users.id')
+				                 ->where('supplier_reseller.supplier_id', '=', $userid);
+				        })
+        			->get();
+    }
+
+    public function mySuppliers($type, $userid)
+    {
+    	$instance = $this->getNewInstance();
+        return DB::table('users')
+        			->where('type', $type)
+        			->join('supplier_reseller', function($join) use ($userid)
+				        {
+				            $join->on('supplier_reseller.supplier_id', '=', 'users.id')
+				                 ->where('supplier_reseller.reseller_id', '=', $userid);
+				        })
+        			->get();
     }
 
 }
