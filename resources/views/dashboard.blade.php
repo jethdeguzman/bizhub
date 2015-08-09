b9e18f04ab67ddd0534caec94755b3b5078a1493
Mark Penaranda <markangelpenaranda@gmail.com>
2015-08-09 10:13:22 +0800

    Design Hero

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+
+@section('nav-content')
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav">
+            <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+            <li>
+                <a href="/suppliers">Suppliers</a>
+            </li>
+            <li>
+                <a href="/products">Products</a>
+            </li>
+            <li>
+                <a href="/orders">Orders</a>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a href="/auth/logout"><i class="icon ion-log-out"></i> Logout</a>
+            </li>
+        </ul>
+    </div>
+@endsection
+@section('content')
+    <div class="sub-nav">
+        <div class="container">
+            <div class="row">
+                <ul class="nav nav-pills">
+                  <li role="presentation" class="active"><a href="/products" >Products</a></li>
+                  <li role="presentation"><a href="/products/new">Add New</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <section id="intro" class="intro-section">
+        <div class="container">
+        </div>
+    </section>
+
+@endsection
